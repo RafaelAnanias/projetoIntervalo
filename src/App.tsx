c6cc05f7cba0196f9a6102a2146a5e9077af2f2b
Rafael da Silva Ananias <20241148060016@ifrn.local>
2026-04-13 09:45:56 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Intervalo from './Intervalo';
+import { useState } from 'react';
 
 export default function App() {
+  const [min, setMin] = useState<number>(0)
+  const [max, setMax] = useState<number>(10)
+
+
   return (
     <View style={styles.container}>
 
-    <Intervalo min={10} max={100}></Intervalo>
+      <TextInput style={styles.input} value={min.toString()} keyboardType='numeric'
+        onChangeText={(valor) => {
+          setMin(+valor)
+        }} />
+
+      <TextInput value={max.toString()}
+        keyboardType='numeric'
+        onChangeText={(valor) => {
+          setMax(+valor)
+        }} />
+
+      <Intervalo min={min} max={max}>
+
+      </Intervalo>
 
     </View>
   );
@@ -15,7 +33,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input: {
+    borderWidth: 1,
+    marginBottom: 20,
+    borderColor: "#000",
+    fontSize: 20,
+    fontWeight: 500,
+    textAlign: "center",
+
+  }
 });
--- src/Intervalo.tsx
+++ src/Intervalo.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from "react-native"
+import { View, Text, Button, StyleSheet, TextInput } from "react-native"
 import { MinMax } from "./minmax"
-import Aleatorio from "./Aleatorio"
+//import Aleatorio from "./Aleatorio"
+import React, { useState } from "react"
 
 const gerarAleatorio = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,17 +12,38 @@ const Intervalo = (params: MinMax) => {
     const min = params.min ?? 0
     const max = params.max ?? 100
 
-    const numero = gerarAleatorio(min, max)
+    //const numero = gerarAleatorio(min, max)
+    //let gerado = gerarAleatorio(min,max)
+
+    const [gerado, setGerado] = useState<number | undefined>(undefined)
 
     return (
         <View>
-            <Text> 
-                Min = {min} e Max = {max}
+            <Button title="Gerar número"
+                color="#841584"
+                onPress={() => {
+                    setGerado(gerarAleatorio(min, max))
+                }}
+            />
+
+            <Text style={styles.resultado}    >
+                {gerado ? `Número gerado = ${gerado}`
+                    : ""}
             </Text>
 
-            <Aleatorio numero={numero} />
-        </View>
+        </View >
+
+
     )
 }
+
+
+const styles = StyleSheet.create({
+    resultado: {
+        fontSize: 20,
+        marginTop: 10
+    }
+
+})
 
 export default Intervalo
